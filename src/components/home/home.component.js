d4cc411e0cab87572837f3ace3ef7d5ac96d0445
Hector Garcia <hmgaar@gmail.com>
2019-01-19 13:06:30 -0500

    Created home component

---
+++ src/components/home/home.component.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import logo from 'src/logo.svg'
+class HomeComponent extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            BRING IT HOME BABY MIAMI BITCOIN HACKATHON
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
