63c380a7a97fca57eeeda00de7dac6f1deb4b4a6
Hector Garcia <hmgaar@gmail.com>
2019-01-19 15:09:59 -0500

    Added nav links to test each page - this will be removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
 import { AppRouter } from './routes/app.routes'
 import './App.css'
 
@@ -10,6 +10,28 @@ class App extends Component {
         <BrowserRouter>
           <div className="app-component">
             <AppRouter/>
+              <div className="nav-links">
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/donor">Donor</Link>
+                  </li>
+                  <li>
+                    <Link to="/non-profit">Non Profit</Link>
+                  </li>
+                  <li>
+                    <Link to="/supplier">Suppier</Link>
+                  </li>
+                  <li>
+                    <Link to="/donate">Donate</Link>
+                  </li>
+                  <li>
+                    <Link to="/purchase">Purchase</Link>
+                  </li>
+                </ul>
+              </div>
           </div>
         </BrowserRouter>
       </div>
