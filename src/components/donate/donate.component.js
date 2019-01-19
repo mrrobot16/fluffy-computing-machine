7c41dc5866cd17cfbf40d8a6f87d426395171e1d
Chris Scott <cyberstrike@gmail.com>
2019-01-19 18:26:49 -0500

    Donor Dashboard work in progress... (#13)

--- src/components/donate/donate.component.js
+++ src/components/donate/donate.component.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 export class DonateComponent extends Component {
+  sendDonation(){
+    console.log('Sending Donation...');
+  }
   render(){
     return (
       <div className="donate">
-        <h3>Donate Component</h3>
+        <input type="number" placeholder="1.00 BTC"/>
+        <p>$3,750.49 USD</p>
+        <button onClick={this.sendDonation}>Make Donation</button>
       </div>
     )
   }
