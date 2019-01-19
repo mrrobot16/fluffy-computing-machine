17be5b54c5c552c57f8040d2beaa274edabd35f3
Hector Garcia <hmgaar@gmail.com>
2019-01-19 14:43:58 -0500

    Created Non Profit Dashboard component

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-import { HomeComponent, DonorDashboardComponent } from '../components'
+import { HomeComponent, DonorDashboardComponent, NonProfitDashboardComponent } from '../components'
 export class AppRouter extends Component {
   render(){
     return(
       <div>
         <Route exact path="/" component={HomeComponent} />
         <Route path="/donor" component={DonorDashboardComponent}/>
+        <Route path="/non-profit" component={NonProfitDashboardComponent} />
       </div>
     )
   }
