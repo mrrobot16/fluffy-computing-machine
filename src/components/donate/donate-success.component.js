256ac51b9ee318a3080449227486126c47da8af6
Hector Garcia <hmgaar@gmail.com>
2019-01-19 21:39:40 -0500

    Added dummy text to donate-success component

--- src/components/donate/donate-success.component.js
+++ src/components/donate/donate-success.component.js
@@ -8,8 +8,17 @@ export class DonateSuccessComponent extends Component {
         <div className="charity-funded">
           <img src={charityLogo} alt="charityLogo"/>
           <p>Donation successful!</p>
-          <p>You donated 1.00 BTC ($3,750 USD) to<span><strong>The Salvation Army</strong></span></p>
-          <p></p>
+          <p>You donated 1.00 BTC ($3,750 USD) to <span><strong>The Salvation Army</strong></span></p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vivamus non sagittis dolor.
+             Nullam eros eros, dictum vel interdum ac, euismod id leo.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vivamus non sagittis dolor.
+             Nullam eros eros, dictum vel interdum ac, euismod id leo.
+          </p>
         </div>
         <Link to="/donor">Go to Dashboard</Link>
       </div>
