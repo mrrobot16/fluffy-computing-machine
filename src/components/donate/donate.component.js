b5559064df71b327e51fd0687c0eb2cabbde33d3
Chris Scott <cyberstrike@gmail.com>
2019-01-20 09:54:12 -0500

    fixed button

--- src/components/donate/donate.component.js
+++ src/components/donate/donate.component.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 import { NavbarDashboardComponent } from '../header/navbar-dashboard.component'
 
+
 export class DonateComponent extends Component {
-  sendDonation(){
-    console.log('Sending Donation...');
+  constructor (props) {
+    super(props);
+    this.sendDonation = this.sendDonation.bind(this)
   }
-  render(){
+
+  sendDonation () {
+    console.log('Sending Donation...');
+    this.props.history.push('/success')
+  }
+
+  render () {
     return (
       <div className="donate">
         <NavbarDashboardComponent/>
@@ -34,29 +42,49 @@ export class DonateComponent extends Component {
               </div>
             </div>
           </div>
-          <div className="row">
+          <div className="row mb-5">
             <div className="col">
               <div className="card" style={{minHeight: '60vh'}}>
                 <div className="card-body">
                   <div className="text-center">
                     <img height='150' className="my-5" src="https://upload.wikimedia.org/wikipedia/commons/0/0d/Heart_Bitcoin_-_I_love_Bitcoin.png" alt=""/>
                   </div>
+                  <div className="row mb-3">
+                    <div className="col">
+                      <div className="card">
+                        <div className="card-body">
+                          <ul className="list-unstyled">
+                          <li className="media">
+                            <div className="media-body">
+                              <div className="row">
+                                <div className="col">
+                                  <h5 className="mt-0">General Fund</h5>
+                                  Donate directly to the organisation.
+                                </div>
+                              </div>
+                            </div>
+                          </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                   <div className="text-center mb-3">
-                    <div class="btn-group btn-group-toggle btn-block" data-toggle="buttons">
-                      <label class="btn btn-primary active">
-                        <input type="radio" name="options" id="option1" autocomplete="off" checked />
+                    <div className="btn-group btn-group-toggle btn-block" data-toggle="buttons">
+                      <label className="btn btn-outline-primary active">
+                        <input type="radio" name="options" id="option1" autoComplete="off" checked />
                         Once
                       </label>
-                      <label class="btn btn-primary">
-                        <input type="radio" name="options" id="option1" autocomplete="off" />
+                      <label className="btn btn-outline-primary">
+                        <input type="radio" name="options" id="option1" autoComplete="off" />
                         Weekly
                       </label>
-                      <label class="btn btn-primary">
-                        <input type="radio" name="options" id="option2" autocomplete="off" />
+                      <label className="btn btn-outline-primary">
+                        <input type="radio" name="options" id="option2" autoComplete="off" />
                         Monthly
                       </label>
-                      <label class="btn btn-primary">
-                        <input type="radio" name="options" id="option3" autocomplete="off" />
+                      <label className="btn btn-outline-primary">
+                        <input type="radio" name="options" id="option3" autoComplete="off" />
                         Yearly
                       </label>
                     </div>
@@ -71,7 +99,9 @@ export class DonateComponent extends Component {
                     </div>
                   </div>
 
-                  <button onClick={this.sendDonation}><Link to="/success">Make Donation</Link></button>
+                  <button className="btn btn-lg btn-success btn-block" onClick={this.sendDonation}>
+                    Make Donation
+                  </button>
                 </div>
               </div>
             </div>
@@ -81,3 +111,5 @@ export class DonateComponent extends Component {
     )
   }
 }
+
+// export var DonateComponent = withRouter(Donate);
