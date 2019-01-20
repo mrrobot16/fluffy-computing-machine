a47141cc97ec64291828fda345060389ef9ec209
Jedi <jedi@Carloss-MacBook-Pro.local>
2019-01-20 00:59:11 -0500

    BottomNavBarComponent

--- src/components/bottom-navbar/bottom-navbar.component.js
+++ src/components/bottom-navbar/bottom-navbar.component.js
@@ -1,30 +1,63 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './bottom-navbar.component.css'
-import charities from '../../assets/icons/charities.svg'
+import donate from '../../assets/icons/donate.svg'
 import wallet from '../../assets/icons/wallet.svg'
 import activity from '../../assets/icons/activity.svg'
 export class BottomNavBarComponent extends Component {
   render(){
     return (
-      <div className="bottom-navbar-buttons">
-        <div className="bottom-button">
-          <img src={charities} alt="icons"/>
-          <button className="charities">
-            <Link to="/donor">Charities</Link>
-          </button>
-        </div>
-        <div className="bottom-button">
-          <img src={wallet} alt="icons"/>
-          <button className="wallet">
-            <Link to="/wallet">Wallet</Link>
-          </button>
-        </div>
-        <div className="bottom-button">
-          <img src={activity} alt="icons"/>
-          <button className="activity">
-            <Link to="/activity">Activity</Link>
-          </button>
+      <div className="bottom-navbar-buttons fixed-bottom">
+        <div class="container border">
+
+          <div class="row my-3">
+
+            <div class="col">
+              <div className="bottom-button container">
+                <div className="row align-items-start">
+                  <div className="col text-center">
+                    <img src={donate} alt="icons"/>
+                  </div>
+                  <div className="col">
+                    <button className="donate text-center">
+                      <Link to="/donor">Donate</Link>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div className="bottom-button container">
+                <div className="row align-items-start">
+                  <div className="col text-center">
+                    <img src={wallet} alt="icons"/>
+                  </div>
+                  <div className="col">
+                    <button className="wallet text-center">
+                      <Link to="/wallet">Wallet</Link>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div className="bottom-button container">
+                <div className="row align-items-start">
+                  <div className="col text-center">
+                    <img src={activity} alt="icons"/>
+                  </div>
+                  <div className="col">
+                    <button className="activity text-center">
+                      <Link to="/activity">Activity</Link>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     )
