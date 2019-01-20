80080f4b7f4455d2c2514b0ff877cbedf7dc644e
Hector Garcia <hmgaar@gmail.com>
2019-01-19 23:12:21 -0500

    clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './routes/app.routes'
 import { HeaderComponent } from './components'
 
@@ -17,31 +17,6 @@ class App extends Component {
           <div className="app-component container">
             <HeaderComponent/>
             <AppRouter/>
-              <div className="nav-links">
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/donor">Donor</Link>
-                  </li>
-                  <li>
-                    <Link to="/non-profit">Non Profit</Link>
-                  </li>
-                  <li>
-                    <Link to="/supplier">Supplier</Link>
-                  </li>
-                  <li>
-                    <Link to="/donate">Donate</Link>
-                  </li>
-                  <li>
-                    <Link to="/purchase">Purchase</Link>
-                  </li>
-                  <li>
-                    <Link to="/activity">Donation Activity</Link>
-                  </li>
-                </ul>
-              </div>
           </div>
         </BrowserRouter>
       </div>
