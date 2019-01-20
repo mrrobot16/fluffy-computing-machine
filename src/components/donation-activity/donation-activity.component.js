6b46cff2f8cab90f84348cbdf81f7d1fb8f77194
Hector Garcia <hmgaar@gmail.com>
2019-01-19 23:26:57 -0500

    fixes in donation activity component

--- src/components/donation-activity/donation-activity.component.js
+++ src/components/donation-activity/donation-activity.component.js
@@ -9,18 +9,18 @@ export class DonationActivityComponent extends Component {
         <div className="container">
           <div className="row">
             <div className="col-sm">
-              <img src={salvationArmyImg} width="162" height="162"></img>
+              <img src={salvationArmyImg} width="162" height="162" alt="salvationArmyImg"></img>
             </div>
             <div className="col-sm">
-              <h3 style="{{ color: #749F77 }}">Donation Update</h3>
+              <p>Donation Update</p>
               <p>The 1.00 BTC ($3,750 USD) you donated to <b>The Salvation Army</b> has been used</p>
             </div>
             <div className="col-sm">
               <h3>Donation activity:</h3>
-              <img src={firstAidImage} width="107" height="95"></img>
-            </div>
-            <div className="col-sm">
-              <b><a href="https://example.com">Go To Dashboard</a></b>
+              <img src={firstAidImage} width="107" height="95" alt="firstAidImage"></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam.</p>
             </div>
           </div>
         </div>
