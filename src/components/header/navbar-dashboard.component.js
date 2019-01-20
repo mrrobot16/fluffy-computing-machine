385b5a3e9f8bc352caebc570ba2e519607c478bd
Chris Scott <cyberstrike@gmail.com>
2019-01-20 10:53:25 -0500

    non profit dash

--- src/components/header/navbar-dashboard.component.js
+++ src/components/header/navbar-dashboard.component.js
@@ -25,12 +25,6 @@ export class NavbarDashboardComponent extends Component {
               <li className="nav-item active">
                 <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">Disabled</a>
-              </li>
             </ul>
           </div>
         </nav>
--- src/components/non-profit-dashboard/non-profit-dashboard.component.js
+++ src/components/non-profit-dashboard/non-profit-dashboard.component.js
@@ -1,68 +1,131 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import { NavbarDashboardComponent } from '../header/navbar-dashboard.component'
+import wallet from '../../assets/icons/wallet.svg'
+import {GearIcon} from '../shared/gear-icon.component'
 
 export class NonProfitDashboardComponent extends Component {
   render(){
     return (
       <div className="non-profit-dashboard">
-        <div className="container">
-
-          <div className="row">
-
-            <div className="col-sm">
-              <h3 className="text-center">Your current balance</h3>
-            </div>
-
-            <div className="col-sm container">
-
-              <div className="container border">
-
-                <div className="row">
-                  <div className="col-4">
-                    <img className="pr-1" height="45" src="https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg" alt=""/>
-                  </div>
-
-                  <div className="col-4">
-                    <h1>737.73</h1>
-                  </div>
-
-                  <div className="col-4">
-                    <h4 className="text-right">BTC</h4>
+      <NavbarDashboardComponent/>
+      <div className="container-full">
+        <div className="col bg-primary" style={{height: '15vh', marginBottom: '-10vh'}} >
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="col-12 col-xl-6" style={{minHeight: "10vh"}}>
+              <div className="row mb-3">
+                <div className="card w-100">
+                  <div className="card-body">
+                    <div className="card-text text-center">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/1/1a/EFF_Logo.svg" className="" alt="..."/>
+                      <h3 className="d-flex align-items-center justify-content-center m-0">
+                        Electronic Frontier Foundation
+                      </h3>
+                    </div>
                   </div>
                 </div>
-
               </div>
-
-              <div className="col">
-                <h3 className="float-right">$2,729,601.00</h3>
-              </div>
-
             </div>
-
-            <div className="col container border">
-              <div className="col">
-                <span>31.23 BTC</span>
-                <div></div>
+            <div className="col-12 col-xl-6" style={{minHeight: "10vh"}}>
+              <div className="row mb-3">
+                <div className="card w-100">
+                  <div className="card-body">
+                    <div className="card-text wallet-card-text">
+                      <img className="float-left" src={wallet} style={{height: '1.5em'}} alt=""/>
+                      <h3 className="d-flex align-items-center justify-content-center m-0">
+                        <img className="pr-1" height="25" src="https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg" alt=""/>
+                        <strong className="d-inline btc-amount">2.32</strong>
+                        <span className="d-inline usd-amount">($8,594)</span>
+                      </h3>
+                      <Link to='/wallet' className="configureIcon" >
+                        <GearIcon/>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="col"></div>
-              <div className="col"></div>
             </div>
-
           </div>
-
-          <div className="col container">
-            <div className="col text-center my-3">
-              <button className="btn btn-primary .btn-block" type="submit">Place an order</button>
-            </div>
-            <div className="col text-center my-3">
-              <button className="btn btn-primary .btn-block" type="submit">Create Campaign</button>
-            </div>
-
-            <div>
-              <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</span>
+        </div>
+        {/* Active Campaings */}
+        <h5>Campaigns</h5>
+        <div className="row mb-3">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <ul className="list-unstyled">
+                <li className="media border-bottom py-4">
+                  <div className="media-body">
+                    <div className="row">
+                      <div className="col-9">
+                        <h5 className="mt-0 mb-1">General Fund</h5>
+                        <div className="progress">
+                          <div className="progress-bar" role="progressbar" style={{width: '25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                        </div>
+                      </div>
+                      <div className="col-3 d-flex align-items-center justify-content-center">
+                        $15,000
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="media pt-4">
+                  <div className="media-body">
+                    <div className="row">
+                      <div className="col-9">
+                        <h5 className="mt-0 mb-1">Net Neutrality Impact Study</h5>
+                        <div className="progress">
+                          <div className="progress-bar" role="progressbar" style={{width: '75%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                        </div>
+                      </div>
+                      <div className="col-3 d-flex align-items-center justify-content-center">
+                        <div className="row">
+                          <div className="col-12">
+                            $55,000
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                </ul>
+              </div>
+              <div class="card-footer text-muted bg-white text-center">
+                <a href="/">
+                    View Past Campaigns
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
+
+        {/* Charity Activity */}
+        <div className="row mb-3">
+          <div className="col">
+            <h5>Activity</h5>
+            <ul className="list-group">
+              <li className="list-group-item">
+                Purchased Pumpkin Spice Lattes
+                <small className="float-right">
+                  1 Week Ago
+                </small>
+              </li>
+              <li className="list-group-item">
+                Completed VPN Awareness Camp...
+                <small className="float-right">
+                  2 Week Ago
+                </small>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
